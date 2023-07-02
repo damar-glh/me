14cbd7479771bad68919d82e6afd97f5eeb288d8
damara <damarmuhamad13@gmail.com>
2023-07-02 20:55:24 +0700

    update: header-component&toggle-navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,12 +11,41 @@ const Header = () => {
                 <div className="nav__menu">
                     <ul className="nav__list grid">
                         <li className="nav__item">
-                            <a href="index.html" className="nav__link">
-                                <i className="uil uil-estate nav__icon"></i>Home
+                            <a href="#home" className="nav__link">
+                                <i className="uil uil-estate nav__icon"></i> Home
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#about" className="nav__link">
+                                <i className="uil uil-user nav__icon"></i> About
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#skills" className="nav__link">
+                                <i className="uil uil-file-alt nav__icon"></i> Skills
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#services" className="nav__link">
+                                <i className="uil uil-briefcase-alt nav__icon"></i> Services
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#portofolio" className="nav__link">
+                                <i className="uil uil-scenery nav__icon"></i> Portofolio
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#cantact" className="nav__link">
+                                <i className="uil uil-message nav__icon"></i> Contact
                             </a>
                         </li>
                     </ul>
+                    <i class="uil uil-times nav__close"></i>
                 </div>
+                <nav className="nav__toggle">
+                    <i class="uil uil-apps"></i>
+                </nav>
             </nav>
         </header>
     )
