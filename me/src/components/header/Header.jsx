e08e18b710de0f4438b9d5fb6d2ece897c772b4d
damara <damarmuhamad13@gmail.com>
2023-07-02 09:51:30 +0700

    update:css-variables&general-css-style

---
+++ me/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+    return(
+        <div className="header">
+            <nav className="nav container">
+                <a href="index.html" className="nav_logo"></a>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
