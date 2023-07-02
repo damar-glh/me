0b998c43952c39faa2347b1d2a1fbdcf045626f5
damara <damarmuhamad13@gmail.com>
2023-07-02 15:40:45 +0700

    update:error-run-program

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,11 +3,21 @@ import "./header.css";
 
 const Header = () => {
     return(
-        <div className="header">
+        <head className="header">
             <nav className="nav container">
-                <a href="index.html" className="nav_logo"></a>
+                <a href="index.html" className="nav__logo">dam.</a>
+
+                <div className="nav__menu">
+                    <ul className="nav__list grid">
+                        <li className="nav__item">
+                            <a href="index.html" className="nav__link">
+                                <i className="uil uil- nav__icon"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
-        </div>
+        </head>
     )
 }
 
