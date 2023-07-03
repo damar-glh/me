297e9be2cfe0d69fb69177be19a74a7df4a32e77
damara <damarmuhamad13@gmail.com>
2023-07-03 20:09:24 +0700

    update: Home-component

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/invites/contact/?i=1qn7f0ctoxn2r&utm_content=4rioy6h" className="home__social-icon" target="_blank">
+            <i class="uil uil-instagram"></i>
+        </a>
+        <a href="https://m.facebook.com/damar.galih.737001?eav=Afa9DNwI3eeZdM4SEa1m0T6kViMZzc6Ev-QMEYKxKS399U--jRaVJbhvp0rNzTTBRe8&paipv=" className="home__social-icon" target="_blank">
+            <i class="uil uil-facebook-f"></i>
+        </a>
+        <a href="https://github.com/damar-glh" className="home__social-icon" target="_blank">
+            <i class="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
