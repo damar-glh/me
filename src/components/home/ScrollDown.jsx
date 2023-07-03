fd0e0e435cd1cad88d7d8d1bf7e2297d0b899d55
damara <damarmuhamad13@gmail.com>
2023-07-04 00:01:29 +0700

    update: finish-home-component

--- src/components/home/ScrollDown.jsx
+++ src/components/home/ScrollDown.jsx
@@ -25,7 +25,7 @@ const ScrollDown = () => {
                   d="M123.359,79.775l0,72.843"
                   style= {{
                     fill: "none",
-                    stroke: "#000",
+                    stroke: "var(--title-color)",
                     strokeWidth: "20px",
                   }}
                 ></path>
@@ -34,11 +34,13 @@ const ScrollDown = () => {
                   d="M236.717,123.359c0,-62.565 -50.794,-113.359 -113.358,-113.359c-62.565,0 -113.359,50.794 -113.359,113.359l0,143.237c0,62.565 50.794,113.359 113.359,113.359c62.564,0 113.358,-50.794 113.358,-113.359l0,-143.237Z"
                   style= {{
                     fill: "none",
-                    stroke: "#000",
+                    stroke: "var(--title-color)",
                     strokeWidth: "20px",
                   }}
                 ></path>
               </svg>
+              <span className="home__scroll-name">Scroll Down</span>
+              <i class="uil uil-arrow-down home__scroll-arrow"></i>
         </a>
     </div>
   )
