cd89a4bfde256f3413c84e256a7a18c5f6a7a3fd
damara <damarmuhamad13@gmail.com>
2023-07-05 18:30:27 +0700

    update: add-services-component

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,128 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <section className="services section" id="services">
+        <h2 className="section__title">Services</h2>
+        <span className="section__subtitle">What i offer</span>
+        <div className="services__container container grid">
+            <div className="services__content">
+                <div>
+                    <i className="uil uil services__icon"></i>
+                    <h3 className="services__title"></h3>
+                </div>
+                <span className="services__button">View More <i className="uil uil-arrow-right services__button-icon"></i></span>
+                <div className="services__modal">
+                    <div className="services__modal-content">
+                        <i className="uil uil-times services__modal-close"></i>
+                        <h3 className="services__modal-title"></h3>
+                        <p className="services__modal-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, exercitationem. Maxime, nam? Facere, et incidunt.</p>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-services">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
+                            </li>
+                        </ul>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-services">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
+                            </li>
+                        </ul>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-services">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
+                            </li>
+                        </ul>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-services">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="services__content">
+                <div>
+                    <i className="uil uil services__icon"></i>
+                    <h3 className="services__title"></h3>
+                </div>
+                <span className="services__button">View More <i className="uil uil-arrow-right services__button-icon"></i></span>
+                <div className="services__modal">
+                    <div className="services__modal-content">
+                        <i className="uil uil-times services__modal-close"></i>
+                        <h3 className="services__modal-title"></h3>
+                        <p className="services__modal-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, exercitationem. Maxime, nam? Facere, et incidunt.</p>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-services">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
+                            </li>
+                        </ul>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-services">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
+                            </li>
+                        </ul>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-services">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
+                            </li>
+                        </ul>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-services">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="services__content">
+                <div>
+                    <i className="uil uil services__icon"></i>
+                    <h3 className="services__title"></h3>
+                </div>
+                <span className="services__button">View More <i className="uil uil-arrow-right services__button-icon"></i></span>
+                <div className="services__modal">
+                    <div className="services__modal-content">
+                        <i className="uil uil-times services__modal-close"></i>
+                        <h3 className="services__modal-title"></h3>
+                        <p className="services__modal-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, exercitationem. Maxime, nam? Facere, et incidunt.</p>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-services">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
+                            </li>
+                        </ul>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-services">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
+                            </li>
+                        </ul>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-services">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
+                            </li>
+                        </ul>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-services">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Services
