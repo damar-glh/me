8cdac257a4536260c50f677154a1799f4672f120
damara <damarmuhamad13@gmail.com>
2023-07-06 20:35:38 +0700

    update: update-class-to-className

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -43,10 +43,10 @@ const Header = () => {
                             </a>
                         </li>
                     </ul>
-                    <i class="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
+                    <i className="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
                 </div>
                 <nav className="nav__toggle" onClick={() => showMenu(!Toggle)}>
-                    <i class="uil uil-apps"></i>
+                    <i className="uil uil-apps"></i>
                 </nav>
             </nav>
         </header>
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -4,16 +4,16 @@ const Social = () => {
   return (
     <div className="home__social">
         <a href="https://www.instagram.com/invites/contact/?i=1qn7f0ctoxn2r&utm_content=4rioy6h" className="home__social-icon" target="_blank" rel="noreferrer">
-            <i class="uil uil-instagram"></i>
+            <i className="uil uil-instagram"></i>
         </a>
         <a href="https://m.facebook.com/damar.galih.737001?eav=Afa9DNwI3eeZdM4SEa1m0T6kViMZzc6Ev-QMEYKxKS399U--jRaVJbhvp0rNzTTBRe8&paipv=" className="home__social-icon" target="_blank" rel="noreferrer">
-            <i class="uil uil-facebook-f"></i>
+            <i className="uil uil-facebook-f"></i>
         </a>
         <a href="https://github.com/damar-glh" className="home__social-icon" target="_blank" rel="noreferrer">
-            <i class="uil uil-github-alt"></i>
+            <i className="uil uil-github-alt"></i>
         </a>
         <a href="https://wa.me/qr/Q3ZAX5TFAH3HI1" className="home__social-icon" target="_blank" rel="noreferrer">
-            <i class="uil uil-whatsapp"></i>
+            <i className="uil uil-whatsapp"></i>
         </a>
     </div>
   )
