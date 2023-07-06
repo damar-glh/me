9bcbdbd21d76364f3b9be516bab4f7306f180332
damara <damarmuhamad13@gmail.com>
2023-07-06 20:27:53 +0700

    update: add-services-component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -54,6 +54,44 @@ const Services = () => {
                     <i className="uil uil-arrow services__icon"></i>
                     <h3 className="services__title">Ui/Ux <br /> Designer</h3>
                 </div>
+                <span className="services__button" onClick={() => toggleTab(3)}>View More <i className="uil uil-arrow-right services__button-icon"></i></span>
+                <div className={toggleState === 3 ? "services__modal active-modal" : "services__modal"}>
+                    <div className="services__modal-content">
+                        <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+                        <h3 className="services__modal-title">Visual Designer</h3>
+                        <p className="services__modal-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, exercitationem. Maxime, nam? Facere, et incidunt.</p>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-services">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
+                            </li>
+                        </ul>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-services">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
+                            </li>
+                        </ul>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-services">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
+                            </li>
+                        </ul>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-services">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="services__content">
+                <div>
+                    <i className="uil uil-arrow services__icon"></i>
+                    <h3 className="services__title">Ui/Ux <br /> Designer</h3>
+                </div>
                 <span className="services__button" onClick={() => toggleTab(2)}>View More <i className="uil uil-arrow-right services__button-icon"></i></span>
                 <div className={toggleState === 2 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
