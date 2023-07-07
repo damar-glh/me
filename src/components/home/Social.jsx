4ae50cb1c9e0921963cecadade864e70600afe40
damara <damarmuhamad13@gmail.com>
2023-07-07 19:05:19 +0700

    update: social-component

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -6,9 +6,6 @@ const Social = () => {
         <a href="https://www.instagram.com/invites/contact/?i=1qn7f0ctoxn2r&utm_content=4rioy6h" className="home__social-icon" target="_blank" rel="noreferrer">
             <i className="uil uil-instagram"></i>
         </a>
-        <a href="https://m.facebook.com/damar.galih.737001?eav=Afa9DNwI3eeZdM4SEa1m0T6kViMZzc6Ev-QMEYKxKS399U--jRaVJbhvp0rNzTTBRe8&paipv=" className="home__social-icon" target="_blank" rel="noreferrer">
-            <i className="uil uil-facebook-f"></i>
-        </a>
         <a href="https://github.com/damar-glh" className="home__social-icon" target="_blank" rel="noreferrer">
             <i className="uil uil-github-alt"></i>
         </a>
