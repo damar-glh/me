d8dc8f5fa2c063f5f5003053be64771c755c5b08
damara <damarmuhamad13@gmail.com>
2023-07-07 11:20:49 +0700

    update: add-qualification-component

---
+++ src/components/qualification/Qualification.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import './qualification.css';
+
+const Qualification = () => {
+  return (
+    <section className="qualification section">
+        <h2 className="section__title">Qualification</h2>
+        <span className="section__subtitle">My personal journey</span>
+        <div className="qualification__container container">
+            <div className="qualification__tabs">
+                <div className="qualification__button qualification__active button--flex">
+                    <i className="uil uil-graduation-cap qualification__icon"></i> Education
+                </div>
+                <div className="qualification__button button--flex">
+                    <i className="uil uil-briefcase-alt qualification__icon"></i> Experience
+                </div>
+            </div>
+            <div className="qualification__sections">
+                <div className="qualification__content">
+                    <div className="qualification__data">
+                        <div className="">
+                            <h3 className="qualification__title">Web Development</h3>
+                            <span className="qualification__subtitle">Spain - Institute</span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calendar-alt"></i> 2022 - Present
+                            </div>
+                        </div>
+                        <div className="">
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+                    <div className="qualification__data">
+                        <div className=""></div>
+                        <div className="">
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                        <div className="">
+                            <h3 className="qualification__title">Cloud Learning</h3>
+                            <span className="qualification__subtitle">Spain - Institute</span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calendar-alt"></i> 2022 - Present
+                            </div>
+                        </div>
+                    </div>
+                    <div className="qualification__data">
+                        <div className="">
+                            <h3 className="qualification__title">Web Development</h3>
+                            <span className="qualification__subtitle">Spain - Institute</span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calendar-alt"></i> 2022 - Present
+                            </div>
+                        </div>
+                        <div className="">
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+                    <div className="qualification__data">
+                        <div className=""></div>
+                        <div className="">
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                        <div className="">
+                            <h3 className="qualification__title">Cloud Learning</h3>
+                            <span className="qualification__subtitle">Spain - Institute</span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calendar-alt"></i> 2022 - Present
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Qualification
