588fc72603b957cd9e4dd8252ee45447fa647da7
damara <damarmuhamad13@gmail.com>
2023-07-07 08:31:30 +0700

    update: finish-services-component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -13,91 +13,47 @@ const Services = () => {
         <div className="services__container container grid">
             <div className="services__content">
                 <div>
-                    <i className="uil uil-web-grid services__icon"></i>
-                    <h3 className="services__title">Product <br /> Developer</h3>
+                    <i className="uil uil-arrow services__icon"></i>
+                    <h3 className="services__title">Software <br /> Developer</h3>
                 </div>
                 <span className="services__button" onClick={() => toggleTab(1)}>View More <i className="uil uil-arrow-right services__button-icon"></i></span>
                 <div className={toggleState === 1 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
                         <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
-                        <h3 className="services__modal-title">Product Developer</h3>
-                        <p className="services__modal-description">As a product developer in the field of web development, I have creative and innovative skills in developing captivating digital products. I work collaboratively with a team of developers, designers, and other stakeholders, with a primary focus on user satisfaction. I always strive to deliver products with optimal performance, keeping up with current trends and being responsive to user feedback. Throughout the development process, I pay attention to the smallest details and act as a reliable problem solver. With good time management abilities, I produce innovative and high-quality web products.</p>
+                        <h3 className="services__modal-title">Software Developer</h3>
+                        <p className="services__modal-description">As a software developer, I possess creative and innovative skills in crafting compelling software products. I actively collaborate with a team of developers, designers, and stakeholders, placing a strong emphasis on user satisfaction. My main objective is to deliver products that perform optimally, while staying abreast of current industry trends and being responsive to user feedback. Throughout the software development lifecycle, I meticulously attend to even the minutest details and serve as a dependable problem solver. With proficient time management abilities, I consistently produce innovative and top-notch software products.</p>
                     </div>
                 </div>
             </div>
             <div className="services__content">
                 <div>
-                    <i className="uil uil-arrow services__icon"></i>
-                    <h3 className="services__title">Ui/Ux <br /> Designer</h3>
+                    <i className="uil uil-cloud-computing services__icon"></i>
+                    <h3 className="services__title">Cloud <br /> Computing</h3>
                 </div>
                 <span className="services__button" onClick={() => toggleTab(3)}>View More <i className="uil uil-arrow-right services__button-icon"></i></span>
                 <div className={toggleState === 3 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
                         <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
-                        <h3 className="services__modal-title">Visual Designer</h3>
-                        <p className="services__modal-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, exercitationem. Maxime, nam? Facere, et incidunt.</p>
-                        <ul className="services__modal-services grid">
-                            <li className="services__modal-services">
-                                <i className="uil uil-check-circle services__modal-icon"></i>
-                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
-                            </li>
-                        </ul>
-                        <ul className="services__modal-services grid">
-                            <li className="services__modal-services">
-                                <i className="uil uil-check-circle services__modal-icon"></i>
-                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
-                            </li>
-                        </ul>
-                        <ul className="services__modal-services grid">
-                            <li className="services__modal-services">
-                                <i className="uil uil-check-circle services__modal-icon"></i>
-                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
-                            </li>
-                        </ul>
-                        <ul className="services__modal-services grid">
-                            <li className="services__modal-services">
-                                <i className="uil uil-check-circle services__modal-icon"></i>
-                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
-                            </li>
-                        </ul>
+                        <h3 className="services__modal-title">Cloud Computing</h3>
+                        <p className="services__modal-description">
+                        As a provider of cloud computing services, I offer reliable and innovative solutions that allow you to optimize the management of your IT infrastructure. With an emphasis on flexibility, security, and scalability, I provide a platform that enables you to efficiently allocate computing resources, securely store and manage your data, and run business applications with optimal performance. With responsive service and dependable technical support, I am committed to meeting your needs and assisting you in your digital transformation journey.
+                        </p>
                     </div>
                 </div>
             </div>
             <div className="services__content">
                 <div>
-                    <i className="uil uil-arrow services__icon"></i>
-                    <h3 className="services__title">Ui/Ux <br /> Designer</h3>
+                    <i className="uil uil-web-grid services__icon"></i>
+                    <h3 className="services__title">Project <br /> Management</h3>
                 </div>
                 <span className="services__button" onClick={() => toggleTab(2)}>View More <i className="uil uil-arrow-right services__button-icon"></i></span>
                 <div className={toggleState === 2 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
                         <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
-                        <h3 className="services__modal-title">Visual Designer</h3>
-                        <p className="services__modal-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, exercitationem. Maxime, nam? Facere, et incidunt.</p>
-                        <ul className="services__modal-services grid">
-                            <li className="services__modal-services">
-                                <i className="uil uil-check-circle services__modal-icon"></i>
-                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
-                            </li>
-                        </ul>
-                        <ul className="services__modal-services grid">
-                            <li className="services__modal-services">
-                                <i className="uil uil-check-circle services__modal-icon"></i>
-                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
-                            </li>
-                        </ul>
-                        <ul className="services__modal-services grid">
-                            <li className="services__modal-services">
-                                <i className="uil uil-check-circle services__modal-icon"></i>
-                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
-                            </li>
-                        </ul>
-                        <ul className="services__modal-services grid">
-                            <li className="services__modal-services">
-                                <i className="uil uil-check-circle services__modal-icon"></i>
-                                <p className="services__modal-info">Lorem ipsum dolor sit amet.</p>
-                            </li>
-                        </ul>
+                        <h3 className="services__modal-title">Project Management</h3>
+                        <p className="services__modal-description">
+                        As a provider of project management services, I, as the client, can rely on my expertise and experience in successfully managing projects. In my role as a project manager, I offer a collaborative approach and prioritize client satisfaction. I understand the importance of time, budget, and quality in every project I handle. With strong communication skills and effective leadership abilities, I ensure that the team works efficiently and in an organized manner. I prioritize creative problem-solving, pay attention to detail, and remain committed to delivering innovative and high-quality end products. With my support as a provider of project management services, you can be confident that your project will be executed with success and high efficiency.
+                        </p>
                     </div>
                 </div>
             </div>
