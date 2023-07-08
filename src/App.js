90901ad2d1d2e405d76af5a7d4dbf3548b78d656
damara <damarmuhamad13@gmail.com>
2023-07-08 18:12:26 +0700

    update: add-testimonial-component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
 import Qualification from './components/qualification/Qualification';
+import Testimonials from './components/testimonials/Testimonials';
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
       <Skills />
       <Services />
       <Qualification />
+      <Testimonials />
     </main>
     </>
   )
