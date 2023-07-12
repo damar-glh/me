77665d05f6032a892f761e4d8c4bcb3d74580f65
damara <damarmuhamad13@gmail.com>
2023-07-12 18:30:33 +0700

    update: add-footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -19,14 +19,14 @@ const Footer = () => {
             </ul>
             <div className="footer__social">
             <a href="https://www.instagram.com/invites/contact/?i=1qn7f0ctoxn2r&utm_content=4rioy6h" className="footer__social-link" target="_blank" rel="noreferrer">
-            <i className="uil uil-instagram"></i>
-        </a>
-        <a href="https://github.com/damar-glh" className="footer__social-link" target="_blank" rel="noreferrer">
-            <i className="uil uil-github-alt"></i>
-        </a>
-        <a href="https://wa.me/qr/Q3ZAX5TFAH3HI1" className="footer__social-link" target="_blank" rel="noreferrer">
-            <i className="uil uil-whatsapp"></i>
-        </a>
+            <i className="bx bxl-instagram"></i>
+            </a>
+            <a href="https://github.com/damar-glh" className="footer__social-link" target="_blank" rel="noreferrer">
+            <i className="bx bxl-github"></i>
+            </a>
+            <a href="https://wa.me/qr/Q3ZAX5TFAH3HI1" className="footer__social-link" target="_blank" rel="noreferrer">
+            <i className="bx bxl-whatsapp"></i>
+            </a>
             </div>
             <span className="footer__copy">&#169; Created by damar.glh__</span>
         </div>
