197b4791f29f0abf7191aa8f4c6a9663ad0387b0
damara <damarmuhamad13@gmail.com>
2023-07-12 18:53:42 +0700

    update: add-scrollup

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Qualification from './components/qualification/Qualification';
 import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
+import ScrollUp from './components/scrollup/ScrollUp';
 
 const App = () => {
   return (
@@ -23,8 +24,9 @@ const App = () => {
       <Qualification />
       <Testimonials />
       <Contact />
-      <Footer />
     </main>
+    <Footer />
+    <ScrollUp />
     </>
   )
 }
---
+++ src/components/scrollup/ScrollUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './scrollup.css'
+
+const ScrollUp = () => {
+  return (
+    <a href="#" className="scrollup">
+        <i className="uil uil-arrow-up scrollup__icon"></i>
+    </a>
+  )
+}
+
+export default ScrollUp
