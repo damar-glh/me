81897cceafed044ae9cda1b3ae3059f6d3e2df91
damara <damarmuhamad13@gmail.com>
2023-07-13 17:51:12 +0700

    update: add-portofolio-page

---
+++ src/components/work/Work.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './work.css'
+import Works from './Works'
+
+const Work = () => {
+  return (
+    <section className="work section" id="portofolio">
+        <h2 className="section__title">Portofolio</h2>
+        <span className="section__subtitle">Most recent work</span>
+        <Works />
+    </section>
+  )
+}
+
+export default Work
