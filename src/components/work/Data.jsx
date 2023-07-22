3592566fa0b6df50cca45ef8ce3c8bd4e9bab501
damara <damarmuhamad13@gmail.com>
2023-07-22 19:30:46 +0700

    update: portofolio-page

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,14 +1,14 @@
-import Work1 from "../../assets/work1.jpg";
-import Work2 from "../../assets/work2.jpg";
+import Work1 from "../../assets/coca-cola.png";
+import Work2 from "../../assets/work5.jpg";
 import Work3 from "../../assets/work3.jpg";
 import Work4 from "../../assets/work4.jpg";
-import Work5 from "../../assets/work5.jpg";
+import Work5 from "../../assets/shoes.png";
 
 export const projectsData = [
   {
     id: 1,
     image: Work1,
-    title: "Web design",
+    title: "Coca-Cola Web",
     category: "web",
   },
   {
@@ -32,7 +32,7 @@ export const projectsData = [
   {
     id: 5,
     image: Work5,
-    title: "Web design",
+    title: "Shoes Web",
     category: "web",
   },
 ];
