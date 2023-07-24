814312f28d2940369d44435ea4efc03eaa613709
damara <damarmuhamad13@gmail.com>
2023-07-24 23:04:48 +0700

    update: page-skills

--- src/components/skills/Lecskills.jsx
+++ src/components/skills/Lecskills.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import c from '../../assets/c.svg';
 import cpp from '../../assets/cpp.svg';
 import csharp from '../../assets/csharp.svg';
 import php from '../../assets/php.svg';
@@ -9,6 +10,13 @@ const Backend = () => {
         <h3 className="skills__title">Lectures skills</h3>
         <div className="skills__box">
             <div className="skills__group">
+                <div className="skills__data">
+                    <img src={c} alt="" className='skills__component' />
+                    <div>
+                        <h3 className="skills__name">C</h3>
+                        <span className="skills__level">Beginner</span>
+                    </div>
+                </div>
                 <div className="skills__data">
                     <img src={cpp} alt="" className='skills__component' />
                     <div>
