a658cf6dda1ab42463232d7039b3403a78216231
damara <damarmuhamad13@gmail.com>
2023-07-25 10:06:30 +0700

    update: page-work

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -2,7 +2,7 @@ import Work1 from "../../assets/coca-cola.png";
 import Work2 from "../../assets/me.png";
 import Work3 from "../../assets/shoes.png";
 import Work4 from "../../assets/work4.jpg";
-import Work5 from "../../assets/shoes.png";
+import Work5 from "../../assets/work5.jpg";
 
 export const projectsData = [
   {
@@ -14,15 +14,14 @@ export const projectsData = [
   {
     id: 2,
     image: Work2,
-    title: "Portofolio Web",
+    title: "Porto Web",
     category: "web",
   },
   {
     id: 3,
     image: Work3,
     title: "Shoes Web",
-    category: "web
-    ",
+    category: "web",
   },
   {
     id: 4,
