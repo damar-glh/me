3300d0a9256a2d92ba375feccc1f7c06768d1123
damara <damarmuhamad13@gmail.com>
2023-07-25 12:39:10 +0700

    update: page-testi

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -1,26 +1,24 @@
-import Image1 from "../../assets/testimonial1.png";
-import Image2 from "../../assets/testimonial2.png";
-import Image3 from "../../assets/testimonial3.png";
+import user from "../../assets/user-circle.svg";
 
 export const Data = [
     {
       id: 1,
-      image: Image1,
-      title: "Jhon Doe",
+      image: user,
+      title: "Yasin Abdullah",
       description:
         "A really good job, all aspects of the project were followed step by step and with good results.",
     },
     {
       id: 2,
-      image: Image2,
-      title: "Harry Clinton",
+      image: user,
+      title: "Nasir Amin",
       description:
         "A really good job, all aspects of the project were followed step by step and with good results.",
     },
     {
       id: 3,
-      image: Image3,
-      title: "Sara Cill",
+      image: user,
+      title: "Ahmad Mubarok",
       description:
         "A really good job, all aspects of the project were followed step by step and with good results.",
     },
