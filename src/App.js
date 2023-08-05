f084e2c9b82e71bb3b430d9f402c71789f0e982a
damara <damarmuhamad13@gmail.com>
2023-08-05 10:00:26 +0700

    feature: add-loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
@@ -13,6 +13,7 @@ import Footer from './components/footer/Footer';
 import ScrollUp from './components/scrollup/ScrollUp';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   const toggleDarkMode = () => {
@@ -20,31 +21,43 @@ const App = () => {
     localStorage.setItem("darkMode", !isDarkMode ? "enabled" : "disabled");
   };
 
-  useState(() => {
+  useEffect(() => {
     const darkModePreference = localStorage.getItem("darkMode");
     if (darkModePreference === "enabled") {
       setIsDarkMode(true);
     }
+    
+    // Simulate a delay to show the loader
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500); // You can adjust the delay as needed
   }, []);
 
   return (
     <>
-    <Header isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
-    
-    <main className={`main ${isDarkMode ? "dark-mode" : "light-mode"}`}>
-      <Home isDarkMode={isDarkMode}/>
-      <About isDarkMode={isDarkMode} />
-      <Skills isDarkMode={isDarkMode} />
-      <Learning isDarkMode={isDarkMode} />
-      <Qualification isDarkMode={isDarkMode} />
-      <Work isDarkMode={isDarkMode} />
-      <Testimonials isDarkMode={isDarkMode} />
-      <Contact isDarkMode={isDarkMode} />
-    </main>
-    <Footer isDarkMode={isDarkMode} />
-    <ScrollUp />
+      {isLoading ? ( // Display loader while loading
+        <div className="loader">
+          <img src={'./../src/assets/loader.gif'} alt="Loading..." />
+        </div>
+      ) : (
+        <>
+          <Header isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
+          <main className={`main ${isDarkMode ? "dark-mode" : "light-mode"}`}>
+            <Home isDarkMode={isDarkMode} />
+            <About isDarkMode={isDarkMode} />
+            <Skills isDarkMode={isDarkMode} />
+            <Learning isDarkMode={isDarkMode} />
+            <Qualification isDarkMode={isDarkMode} />
+            <Work isDarkMode={isDarkMode} />
+            <Testimonials isDarkMode={isDarkMode} />
+            <Contact isDarkMode={isDarkMode} />
+          </main>
+          <Footer isDarkMode={isDarkMode} />
+          <ScrollUp />
+        </>
+      )}
     </>
-  )
+  );
 }
 
 export default App;
