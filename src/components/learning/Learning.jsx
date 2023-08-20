fdcb6b4f0a3914db8c73b26fbd7cddacbfe89a09
damara <damarmuhamad13@gmail.com>
2023-08-20 22:22:40 +0700

    update: learning-page

--- src/components/learning/Learning.jsx
+++ src/components/learning/Learning.jsx
@@ -38,11 +38,11 @@ const Learning = ({isDarkMode}) => {
     useEffect(() => {
         setAnimatedText('')
         if (toggleState === 1) {
-            animateText(" As a software developer, I possess creative and innovative skills in crafting compelling software products. I actively collaborate with a team of developers, designers, and stakeholders, placing a strong emphasis on user satisfaction. My main objective is to deliver products that perform optimally, while staying abreast of current industry trends and being responsive to user feedback. Throughout the software development lifecycle, I meticulously attend to even the minutest details and serve as a dependable problem solver. With proficient time management abilities, I consistently produce innovative and top-notch software products. ", () => {});
+            animateText(" ", () => {});
         } else if (toggleState === 2) {
-            animateText(" Learning about Update Technology fascinates me with its rapid advancements in the digital world. It opens up opportunities across various industries and keeps me relevant in this ever-changing landscape. Understanding it is crucial for success in the modern era, where technology updates constantly. Exploring this field is an exciting journey of empowerment and real-life solutions, urging me to continuously adapt and stay competitive in the fast-moving industry. It's like an endless adventure, always offering something new to discover. ", () => {});
+            animateText("  ", () => {});
         } else if (toggleState === 3) {
-            animateText(" As a provider of project management services, I, as the client, can rely on my expertise and experience in successfully managing projects. In my role as a project manager, I offer a collaborative approach and prioritize client satisfaction. I understand the importance of time, budget, and quality in every project I handle. With strong communication skills and effective leadership abilities, I ensure that the team works efficiently and in an organized manner. I prioritize creative problem-solving, pay attention to detail, and remain committed to delivering innovative and high-quality end products. With my support as a provider of project management services, you can be confident that your project will be executed with success and high efficiency. ", () => {});
+            animateText(" ", () => {});
         }
     }, [toggleState])
 
@@ -62,7 +62,7 @@ const Learning = ({isDarkMode}) => {
                     <div className="learning__modal-content">
                         <i onClick={() => toggleTab(1)} className="uil uil-times learning__modal-close"></i>
                         <h3 className="learning__modal-title">Software Developer</h3>
-                        <p className="learning__modal-description">{animatedText}</p>
+                        <p className="learning__modal-description">As a software developer, I possess creative and innovative skills in crafting compelling software products. I actively collaborate with a team of developers, designers, and stakeholders, placing a strong emphasis on user satisfaction. My main objective is to deliver products that perform optimally, while staying abreast of current industry trends and being responsive to user feedback. Throughout the software development lifecycle, I meticulously attend to even the minutest details and serve as a dependable problem solver. With proficient time management abilities, I consistently produce innovative and top-notch software products. </p>
                     </div>
                 </div>
             </div>
@@ -76,7 +76,7 @@ const Learning = ({isDarkMode}) => {
                     <div className="learning__modal-content">
                         <i onClick={() => toggleTab(2)} className="uil uil-times learning__modal-close"></i>
                         <h3 className="learning__modal-title">Update Technology</h3>
-                        <p className="learning__modal-description">{animatedText}</p>
+                        <p className="learning__modal-description">Learning about Update Technology fascinates me with its rapid advancements in the digital world. It opens up opportunities across various industries and keeps me relevant in this ever-changing landscape. Understanding it is crucial for success in the modern era, where technology updates constantly. Exploring this field is an exciting journey of empowerment and real-life solutions, urging me to continuously adapt and stay competitive in the fast-moving industry. It's like an endless adventure, always offering something new to discover.</p>
                     </div>
                 </div>
             </div>
@@ -90,7 +90,7 @@ const Learning = ({isDarkMode}) => {
                     <div className="learning__modal-content">
                         <i onClick={() => toggleTab(3)} className="uil uil-times learning__modal-close"></i>
                         <h3 className="learning__modal-title">Project Management</h3>
-                        <p className="learning__modal-description">{animatedText}</p>
+                        <p className="learning__modal-description">As a provider of project management services, I, as the client, can rely on my expertise and experience in successfully managing projects. In my role as a project manager, I offer a collaborative approach and prioritize client satisfaction. I understand the importance of time, budget, and quality in every project I handle. With strong communication skills and effective leadership abilities, I ensure that the team works efficiently and in an organized manner. I prioritize creative problem-solving, pay attention to detail, and remain committed to delivering innovative and high-quality end products. With my support as a provider of project management services, you can be confident that your project will be executed with success and high efficiency. </p>
                     </div>
                 </div>
             </div>
