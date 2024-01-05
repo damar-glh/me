a79853ee763d3aeb9105d4cb6d865d572ca5b79c
damara <damarmuhamad13@gmail.com>
2024-01-05 22:53:23 +0700

    feat: add linkedin in home page

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
+        <a href="https://www.linkedin.com/in/damar-galih-7b5a1124b/" className="home__social-icon" target="_blank" rel="noreferrer">
+            <i className="uil uil-linkedin-alt"></i>
+        </a>
         <a href="https://www.instagram.com/invites/contact/?i=1qn7f0ctoxn2r&utm_content=4rioy6h" className="home__social-icon" target="_blank" rel="noreferrer">
             <i className="uil uil-instagram"></i>
         </a>
         <a href="https://github.com/damar-glh" className="home__social-icon" target="_blank" rel="noreferrer">
             <i className="uil uil-github-alt"></i>
-        </a>
-        <a href="https://wa.me/qr/Q3ZAX5TFAH3HI1" className="home__social-icon" target="_blank" rel="noreferrer">
-            <i className="uil uil-whatsapp"></i>
         </a>
     </div>
   )
