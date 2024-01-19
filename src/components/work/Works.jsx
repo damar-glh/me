98d3e83bb59752fa7c591bd573310a2939f90e4b
damara <damarmuhamad13@gmail.com>
2024-01-19 10:10:14 +0700

    fix: fixing bug popup modal in learning & work page

--- src/components/work/Works.jsx
+++ src/components/work/Works.jsx
@@ -33,7 +33,7 @@ const Works = () => {
       )
       })}
     </div>
-    <div className="work__container container grid" data-aos="zoom-in" data-aos-delay="1500">
+    <div className="work__container container grid" data-aos="zoom-in" data-aos-delay="500">
       {projects.map((item) => {
         return <WorkItem item={item} key={item.id} />
       })}
